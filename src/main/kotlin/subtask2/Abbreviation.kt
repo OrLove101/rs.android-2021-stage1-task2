a11a5c31c3f0f651efbc00155448da0ac3f6f780
OrLove101 <danyazerorebel@gmail.com>
2021-06-10 20:28:25 +0300

    accomplish third subtusk

--- src/main/kotlin/subtask2/Abbreviation.kt
+++ src/main/kotlin/subtask2/Abbreviation.kt
@@ -1,7 +1,6 @@
 package subtask2
 
 class Abbreviation {
-
     fun abbreviationFromA(a: String, b: String): String {
         return if ( a.toUpperCase().filter { b.contains(it) } == b ) "YES" else "NO"
     }
--- src/main/kotlin/subtask3/Blocks.kt
+++ src/main/kotlin/subtask3/Blocks.kt
@@ -1,11 +1,39 @@
 package subtask3
 
+import java.text.SimpleDateFormat
+import java.time.LocalDate
+import java.util.*
 import kotlin.reflect.KClass
 
 class Blocks {
-
-    // TODO: Complete the following function
     fun getData(blockA: Array<*>, blockB: KClass<*>): Any {
-        throw NotImplementedError("Not implemented")
+        when (blockB) {
+            String::class -> {
+                var result = ""
+
+                blockA.forEach { if ( it is String ) result += it }
+                return result
+            }
+            Int::class -> {
+                var result = 0
+
+                blockA.forEach { if ( it is Int ) result += it }
+                return result
+            }
+            LocalDate::class -> {
+                var localDateResult: LocalDate = LocalDate.MIN
+                val sdf = SimpleDateFormat("dd.MM.yyyy")
+                var calendarResult: Calendar = Calendar.getInstance()
+
+                blockA.forEach {
+                    if ( it is LocalDate && it > localDateResult ) {
+                        localDateResult = it
+                    }
+                }
+                calendarResult.set(localDateResult.year, localDateResult.monthValue - 1, localDateResult.dayOfMonth)
+                return if ( localDateResult == LocalDate.MIN ) "" else sdf.format(calendarResult.time)
+            }
+        }
+        return ""
     }
 }
