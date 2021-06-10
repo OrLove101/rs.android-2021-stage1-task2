a2576e01a169fc327e580d28fd337b59d4cc5a4a
OrLove101 <danyazerorebel@gmail.com>
2021-06-10 10:53:40 +0300

    docs: accomplish first task

--- src/main/kotlin/subtask1/DateFormatter.kt
+++ src/main/kotlin/subtask1/DateFormatter.kt
@@ -1,9 +1,47 @@
 package subtask1
 
-class DateFormatter {
+import java.text.DateFormat
+import java.text.ParseException
+import java.text.SimpleDateFormat
+import java.util.*
 
-    // TODO: Complete the following function
+class DateFormatter {
     fun toTextDay(day: String, month: String, year: String): String {
-        throw NotImplementedError("Not implemented")
+        val simpleDateFormatValidator = SimpleDateFormat("dd.MM.yyyy")
+        val simpleDateFormatResult = SimpleDateFormat("dd MMMM, EEEE")
+        val dateResult: Date
+
+        simpleDateFormatValidator.isLenient = false
+        try {
+            dateResult = simpleDateFormatValidator.parse("${day.toString()}.${month.toString()}.${year.toString()}")
+        } catch ( e: ParseException ) {
+            return "Такого дня не существует"
+        }
+        return simpleDateFormatResult.format(dateResult)
     }
+}
+
+fun main(args: Array<String>) {
+    //DATE
+    val date = Date(0)//represent current date in milliseconds from 1 january 1970
+//    print(date)
+
+    //CALENDAR
+    val calendar = Calendar.getInstance()//class to change date dynamically
+//    println(calendar.time)
+
+    calendar.time = date
+//    println(calendar.time)
+
+    //DATE FORMAT (заготовленные форматы)
+//    val dateFormat = DateFormat.getDateInstance(DateFormat.SHORT)
+//    println(dateFormat.format(calendar.time)) //dateFormat.format(date)
+
+    //SIMPLE DATE FORMAT (свой формат или парсинг)
+    val simpleDateFormat = SimpleDateFormat("dd/MM/yyyy")
+//    println(simpleDateFormat.format(calendar.time))
+
+    //REVERSE TRANSFORMATION
+    val newDate = simpleDateFormat.parse("05/12/1985")
+    println(newDate)
 }
